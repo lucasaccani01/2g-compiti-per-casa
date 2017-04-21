f48c403f617f5e0bdcb4b180921e930d3b70ab84
lucasaccani01 <lucasaccani5@gmail.com>
2017-04-21 17:03:16 +0200

    Add files via upload

---
+++ variabile unsigned long long int.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    unsigned long long int max=18446744073709551615;
+    unsigned long long int min=0;
+    cout<<"ecco il minimo:"<<min<<endl;
+    cout<<"ecco il massimo:"<<max<<endl;
+    cout<<"ecco il minimo-1:"<<min-1<<endl;
+    cout<<"ecco il massimo+1:"<<max+1<<endl;
+    return 0;
+}
